1b69026ee36df956a6981ac4df171702970be434
Sergei Seleznev <seleznev@servers.com>
2019-03-03 13:20:47 +0200

    Getting version generator out

--- lib/locatine/version.rb
+++ lib/locatine/version.rb
@@ -1,12 +1,6 @@
 module Locatine
   # constants here...
-  generator = Random.new 42
-  versions = []
-  1000.times do
-    versions.push generator.rand.round(5)
-  end
-  ITERATION = 1
-  VERSION = versions.sort[ITERATION].to_s
+  VERSION = "0.00506"
   NAME = "locatine"
   HOME = File.readable?("#{Dir.pwd}/lib/#{Locatine::NAME}")? "#{Dir.pwd}/lib/#{Locatine::NAME}" : "#{Gem.dir}/gems/#{Locatine::NAME}-#{Locatine::VERSION}/lib/#{Locatine::NAME}"
 end
