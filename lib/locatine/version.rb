169ff292f035aee1b68231cdc6fb35863ba63581
Sergei Seleznev <seleznev@servers.com>
2019-03-05 12:04:58 +0200

    Version is bumped

--- lib/locatine/version.rb
+++ lib/locatine/version.rb
@@ -1,6 +1,6 @@
 module Locatine
   # constants here...
-  VERSION = "0.00695"
+  VERSION = "0.0092"
   NAME = "locatine"
   HOME = File.readable?("#{Dir.pwd}/lib/#{Locatine::NAME}")? "#{Dir.pwd}/lib/#{Locatine::NAME}" : "#{Gem.dir}/gems/#{Locatine::NAME}-#{Locatine::VERSION}/lib/#{Locatine::NAME}"
 end
