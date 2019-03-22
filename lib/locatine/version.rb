b4058a57fb37084600b27731323a44578634909a
Sergei Seleznev <seleznev@servers.com>
2019-03-22 11:09:14 +0200

    Version bump

--- lib/locatine/version.rb
+++ lib/locatine/version.rb
@@ -1,6 +1,6 @@
 module Locatine
   # constants here...
-  VERSION = '0.01454'.freeze
+  VERSION = '0.01546'.freeze
   NAME = 'locatine'.freeze
   HOME = if File.readable?("#{Dir.pwd}/lib/#{Locatine::NAME}")
            "#{Dir.pwd}/lib/#{Locatine::NAME}"
