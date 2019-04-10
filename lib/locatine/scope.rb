b718699f43526bbfab7e8abd89cc0378bdaa6a67
Sergei Seleznev <seleznev@servers.com>
2019-04-10 13:16:30 +0300

    text fixes:

--- lib/locatine/scope.rb
+++ lib/locatine/scope.rb
@@ -9,6 +9,13 @@ module Locatine
       @scope = scope
     end
 
+    ##
+    # Way to show locatine a bunch elements at once. Elements will be taken
+    # from user selection one by one and saved in scope.
+    #
+    # Params:
+    #
+    # +vars+ hash of vars used for dynamic attributes. Same as in :find
     def define(vars = {})
       item = @search.send(:ask, @scope, '', nil, vars)
       return if item[:element].nil?
